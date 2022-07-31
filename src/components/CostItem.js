52bb343bdd4a4cba628f165c4203ed1e304f079e
Aliaksei Skakun <skaliaksei@gmail.com>
2022-08-01 00:00:27 +0300

    feat: useState()

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,17 +1,26 @@
 import './CostItem.css';
 import Card from './Card';
 import CostDate from './CostDate';
+import React, { useState } from 'react';
 
 function CostItem(props) {
+    const [description, setDescription] = useState(props.description);
+
+    const buttonClickHandler = () => {
+        setDescription('new text');
+        console.log(description);
+    };
+
     return (
         <Card className='cost-item'>
             <CostDate date = {props.date}/>
             <div className='cost-item__description'>
-                <h2>{props.description}</h2>
+                <h2>{description}</h2>
                 <div className='cost-item__price'>${props.price}</div>
             </div>
+            <button onClick={buttonClickHandler}>Change description</button>
         </Card>
-    )
-}
+    );
+};
 
 export default CostItem;
